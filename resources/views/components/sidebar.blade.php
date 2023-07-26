8246915ee42e108304b06e5821e7c221437b07e5
diego0023 <diegogalan.0023@gmail.com>
2023-07-26 12:57:27 -0600

    about us pagina

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -6,7 +6,7 @@
         @foreach ($categories as $category)
             <a href="{{ route('by-category', $category) }}"
             class="text-semibold block py-2 px-3 rounded
-                {{ request('category')->slug === $category->slug ?
+                {{ request('category')?->slug === $category->slug ?
                 'bg-blue-600 text-white' : '' }}">
 
                 {{ $category->title }} ( {{ $category->total }} )
