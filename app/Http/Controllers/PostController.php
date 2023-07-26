823f014f54d050a53a48b31b68c106d1cca0cc94
diego0023 <diegogalan.0023@gmail.com>
2023-07-26 11:32:51 -0600

    paginacion en post y pagina individual para cada post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 class PostController extends Controller
@@ -19,7 +20,7 @@ class PostController extends Controller
             ->where('active', '=', 1)
             ->whereDate('published_at', '<', Carbon::now())
             ->orderBy('published_at', 'desc')
-            ->paginate();
+            ->paginate(10);
 
         return view('home', compact('posts'));
     }
@@ -45,7 +46,27 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        if(!$post->active || $post->publish_at > Carbon::now()){
+            throw new NotFoundHttpException();
+        }
+
+        $next = Post::query()
+            ->where('active', true)
+            ->whereDate('published_at', '<=', Carbon::now())
+            ->whereDate('published_at', '<', $post->published_at)
+            ->orderBy('published_at', 'desc')
+            ->limit(1)
+            ->first();
+
+        $prev = Post::query()
+            ->where('active', true)
+            ->whereDate('published_at', '<=', Carbon::now())
+            ->whereDate('published_at', '>', $post->published_at)
+            ->orderBy('published_at', 'asc')
+            ->limit(1)
+            ->first();
+
+        return view('post.view', compact('post', 'prev', 'next'));
     }
 
     /**
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -33,4 +33,12 @@ class Post extends Model
     public function getFormatedDate(){
         return $this->published_at->format('F jS Y');
     }
+
+    public function getThumbnail(){
+        if(str_starts_with($this->thumbnail, 'http')){
+            return $this->thumbnail;
+        }
+
+        return '/storage/'. $this->thumbnail;
+    }
 }
