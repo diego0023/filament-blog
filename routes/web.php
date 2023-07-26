dd64a3728d25188c81127c605ff379d98d29ba67
diego0023 <diegogalan.0023@gmail.com>
2023-07-26 16:26:41 -0600

    singup and login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\SiteController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,20 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 
 Route::get('/',[PostController::class, 'index'])->name('home');
 Route::get('/about-us', [SiteController::class, 'about'])->name('about-us');
